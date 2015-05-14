fab56b1aea7dd555ee788ae2d5cff609939e2e9b
Andrew Thomson <athomsonguy@gmail.com>
2015-05-13 22:58:23 -0700

    improve address.sh.

--- address.sh
+++ address.sh
@@ -1,2 +1,3 @@
-s3cmd sync s3://chomsonforms/address .
-find address/ -type f | xargs jq [.name,.plusname,.addressline1,.addressline2,.city,.state,.zip] | jq @csv
+mkdir -p address
+s3cmd sync s3://chomsonforms/address address
+find address/ -type f | xargs jq -r '[.name,.plusname,.addressline1,.addressline2,.city,.state,.zip] | @csv'
