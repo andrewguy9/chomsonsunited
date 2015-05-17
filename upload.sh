16721039ac3176a46c640e2d57c04cb943e1c93f
Andrew Thomson <athomsonguy@gmail.com>
2015-05-16 21:15:12 -0700

    Apply feedback from spellcheck.net.

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd ../site
-s3cmd -f -P -F --cf-invalidate --cf-invalidate-default-index sync * s3://chomsonsunited
+s3cmd -f -P -F --cf-invalidate --cf-invalidate-default-index sync ./* s3://chomsonsunited
 popd
 repeat --until-success --period 30 -- ./cfdone.sh
