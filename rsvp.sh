524232ba0ef752a64fde1fc7fa5cf98ad6e65eb9
Andrew Thomson <athomsonguy@gmail.com>
2015-05-26 00:15:29 -0700

    make rsvp sort by first member of each submission.

--- rsvp.sh
+++ rsvp.sh
@@ -1,5 +1,5 @@
 mkdir -p rsvp
 s3cmd sync s3://chomsonforms/rsvp rsvp
-find rsvp/ -type f | xargs jq -r 'select(.attending=="true")|.names[]' | sort > attending.txt
-find rsvp/ -type f | xargs jq -r 'select(.attending=="false")|.names[]' | sort > notattending.txt
+find rsvp/ -type f | xargs jq -r 'select(.attending=="true")|.names|@csv' | sort > attending.txt
+find rsvp/ -type f | xargs jq -r 'select(.attending=="false")|.names|@csv' | sort > notattending.txt
 find rsvp/ -type f | xargs jq -r 'select(.attending=="true"and.diet_restrictions!="")|{"names":.names,"diet":.diet_restrictions}' > diet.txt
